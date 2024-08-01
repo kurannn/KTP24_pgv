70695bc8a43db525f9c80861d249ccd7847e2585
Fahrettin Kuran <136595644+kurannn@users.noreply.github.com>
2024-08-01 22:28:25 +0300

    Update ktp24_app.py

--- ktp24_app.py
+++ ktp24_app.py
@@ -1,19 +1,24 @@
-"""
-Created on Wed Jan 17 10:00:57 2024
-@author:  Fahrettin Kuran
-Title:    Turkiye-Specific Ground Motion Model for Peak Ground Velocity (PGV)
-Paper:    Kuran, F., Tanircan, G., Pashaei, E (2024), Comparative analysis of machine learning techniques for predicting peak ground velocity.
-Streamlit app
-"""
+#"""
+#@author:  Fahrettin Kuran
+#Title:    Turkiye-Specific Ground Motion Model for Peak Ground Velocity (PGV)
+#Paper:    Kuran F, Tanircan G, Pashaei E (2024) Developing machine learning-based ground motion models to predict peak ground velocity in Turkiye.
+#Streamlit app
+#"""
 
-import pandas as pd
-import numpy as np
-import pickle
 import streamlit as st
+import joblib
+import numpy as np
+import pandas as pd
 
-with open("ktp24_model.pkl", "rb") as file:
-    model = pickle.load(file)
-    
+
+scaler = joblib.load('scaler.pkl')
+model = joblib.load('ktp24_model.sav')
+
+def normalize_input(user_input, scaler):
+    input_array = np.array(user_input).reshape(1, -1)
+    normalized_input = scaler.transform(input_array)
+    return normalized_input
+
 primaryColor="#426EDA"
 backgroundColor="#FFFFFF"
 secondaryBackgroundColor="#F0F2F6"
@@ -21,7 +26,7 @@ textColor="#31333F"
 font="Sans serif"
 
 col1, col2 = st.columns([8, 2])
-col1.subheader("Turkiye-Specific Ground Motion Model for Peak Ground Velocity (PGV)")
+col1.subheader("Turkiye-Specific Ground Motion Model for Peak Ground Velocity (PGV) (KTP24)")
 col2.image("Kandilli_logo.png", use_column_width=True)
 
 text = """
@@ -34,12 +39,12 @@ st.markdown(f"<p style='line-height: {line_height}; font-size: {font_size};'>{te
 
 st.subheader("Estimator parameters")
 input_info = [
-    "<strong>Mw:</strong> Moment Magnitude",
-    "<strong>Vs30:</strong> Shear wave velocity of the top 30 m of the soil (m/s)",
+    "<strong>M<sub>w</sub>:</strong> Moment Magnitude",
+    "<strong>Vs<sub>30</sub>:</strong> Shear wave velocity of the top 30 m of the soil (m/s)",
     "<strong>δ:</strong> Dip angle (°)",
-    "<strong>Rjb:</strong> Joyner-Boore distance  (km)",
-    "<strong>Rrup:</strong> Rupture distance (km)",
-    "<strong>Repi:</strong> Epicentral distance (km)",
+    "<strong>R<sub>JB</sub>:</strong> Joyner-Boore distance (km)",
+    "<strong>R<sub>rup</sub>:</strong> Rupture distance (km)",
+    "<strong>R<sub>epi</sub>:</strong> Epicentral distance (km)",
     "<strong>SoF:</strong> Style-of-faulting"
 ]
 
@@ -47,62 +52,52 @@ line_height = "0.4"
 font_size = "14px"
 for line in input_info:
     st.markdown(f"<p style='line-height: {line_height}; font-size: {font_size};'>{line}</p>", unsafe_allow_html=True)
-    
 
-def user_input_features():
-    st.sidebar.markdown("<h1 style='text-align: center; font-size: 16px;'>Define estimator parameters</h1>", unsafe_allow_html=True)
-    Mw = st.sidebar.slider("Mw", 3.5, 7.8, step=0.1)
-    VS30 = st.sidebar.slider("Vs30", 131, 1862)
+# Streamlit app
+def main():
+
+    # User inputs
+    Mw = st.sidebar.slider(r"$M_{w}$", 3.5, 7.8, step=0.1)
+    Vs30 = st.sidebar.slider(r"$Vs_{30}$", 131, 1862)
     Dip = st.sidebar.slider("δ", 30, 90)
-    Rjb = st.sidebar.slider("Rjb", 0, 200)
-    Rrup = st.sidebar.slider("Rrup", 0, 200)
-    Repi = st.sidebar.slider("Repi", 0, 200)
-    SOF = st.sidebar.selectbox("SoF", ["Strike-slip", "Normal", "Reverse"])
+    Rjb = st.sidebar.slider(r"$R_{JB}$", 0.0, 200.0, step=0.01)
+    Rrup = st.sidebar.slider(r"$R_{rup}$", 0.0, 200.0, step=0.01)
+    Repi = st.sidebar.slider(r"$R_{epi}$", 0.0, 200.0, step=0.01)
+    SoF = st.sidebar.selectbox(r"$SoF$", ["Strike-slip", "Normal", "Reverse"])
 
+    normal = 1 if SoF == "Normal" else 0
+    strike_slip = 1 if SoF == "Strike-slip" else 0
+    thrust = 1 if SoF == "Reverse" else 0
 
-    sof_encoding = {'Strike-slip': 0.51439292, 'Normal': 0.44248432, 'Reverse': 0.04312276}
+    user_input = [Mw, Vs30, Dip, Rjb, Rrup, Repi, normal, strike_slip, thrust]
 
+    input_summary = pd.DataFrame({
+        'Mw': [Mw],
+        'Vs30': [Vs30],
+        'Dip': [Dip],
+        'Rjb': [Rjb],
+        'Rrup': [Rrup],
+        'Repi': [Repi],
+        'SoF': [SoF]
+    })
 
-    SOF_encoded = sof_encoding.get(SOF, 0)
+    if st.button('Submit'):
+       
+        normalized_input = normalize_input(user_input, scaler)
+        
+        
+        prediction = model.predict(normalized_input)
+        st.subheader('Summary of your inputs')
+        st.table(input_summary)
+        st.write(f'PGV = {prediction[0]:.5f} cm/s')
 
-
-    normalized_inputs = np.array([Mw, VS30, Dip, Rjb, Rrup, Repi, SOF_encoded])
-
-
-    min_values = np.array([3.5000, 131, 10.59000, 0.2919655592323920000, 1.2100000, 0.634047962082121000, 0])
-    max_values = np.array([7.8000, 1862, 90, 199.40012782305000, 199.9867020241710000, 199.986702000, 1])
-
-    normalized_inputs = (normalized_inputs - min_values) / (max_values - min_values)
-
-    actual_values = np.array([Mw, VS30, Dip, Rjb, Rrup, Repi, SOF])
-
-    return normalized_inputs.reshape(1, -1), actual_values
-
-input_features, actual_values = user_input_features()
-
-st.subheader("Summary of your inputs")
-
-input_df = pd.DataFrame({'Estimator parameters': ['Mw', 'Vs30', 'Dip', 'Rjb', 'Rrup', 'Repi', 'SoF'],
-                          'Value': actual_values})
-
-
-input_df_transposed = input_df.T
-
-
-st.write(input_df_transposed)
-
-if st.button("Submit"):
-    prediction = model.predict(input_features)
-    predicted_pgv = max(0, prediction[0])
-
-    st.subheader("Result")
-    st.write(f"PGV = {predicted_pgv:,.5f} cm/s")
+if __name__ == '__main__':
+    main()
 
 
 paper = """
-<strong>Publication:</strong> Kuran, F., Tanircan, G., Pashaei, E (2024), Comparative analysis of machine learning techniques for predicting peak ground velocity.
+<strong>Publication:</strong> Kuran F, Tanircan G, Pashaei E (2024) Developing machine learning-based ground motion models to predict peak ground velocity in Turkiye.
 """
 line_height = "1.2"
 font_size = "14px"
 st.markdown(f"<p style='line-height: {line_height}; font-size: {font_size};'>{paper}</p>", unsafe_allow_html=True)
-
